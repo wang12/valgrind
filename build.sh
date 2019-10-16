ae91818dd464a9748d6dfe768d4943abc8501188
xiaoqiang <wangqiangqiang@kingsoft.com>
2019-10-16 15:51:02 +0800

    build android

--- build.sh
+++ build.sh
@@ -1,15 +1,32 @@
 #!/bin/sh
 export HWKIND=generic
-export AR=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-ar
-export LD=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-ld
-export CC=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-gcc
-export STRIP=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-strip
-make clean
-CPPFLAGS="--sysroot=$ANDROID_NDK/platforms/android-14/arch-arm -DANDROID_HARDWARE_$HWKIND" \
-	CFLAGS="--sysroot=$ANDROID_NDK/platforms/android-14/arch-arm " \
-	LIBS="-L$ANDROID_NDK/platforms/android-14/arch-arm/usr/lib" \
-   ./configure --prefix=/data/local/Inst \
-   --host=armv7-unknown-linux --target=armv7-unknown-linux \
+# 需要先执行Android NDK提供的make_standalone_toolchain.py 生成工具链
+export AR=/Users/xiaoqiang/workspace/android-ndk-arm-14/bin/arm-linux-androideabi-ar
+export LD=/Users/xiaoqiang/workspace/android-ndk-arm-14/bin/arm-linux-androideabi-ld
+export CC=/Users/xiaoqiang/workspace/android-ndk-arm-14/bin/arm-linux-androideabi-gcc
+export CXX=/Users/xiaoqiang/workspace/android-ndk-arm-14/bin/arm-linux-androideabi-g++
+export NM=/Users/xiaoqiang/workspace/android-ndk-arm-14/bin/arm-linux-androideabi-nm
+export STRIP=/Users/xiaoqiang/workspace/android-ndk-arm-14/bin/arm-linux-androideabi-strip
+export RANLIB=/Users/xiaoqiang/workspace/android-ndk-arm-14/bin/arm-linux-androideabi-ranlib
+ make clean
+CPPFLAGS="--sysroot=/Users/xiaoqiang/workspace/android-ndk-arm-14/sysroot -DANDROID_HARDWARE_$HWKIND -D__ANDROID_API__=14" \
+CFLAGS="--sysroot=/Users/xiaoqiang/workspace/android-ndk-arm-14/sysroot -D__ANDROID_API__=14" \
+LIBS="-L/Users/xiaoqiang/workspace/android-ndk-arm-14/sysroot/usr/lib" \
+./configure --prefix=/data/local/tmp/local/Inst \
+   --host=armv7-unknown-linux \
    --target=arm-linux-androideabi --with-tmpdir=/sdcard
-make -j4
+
+make -j4 TARGET=ARMV7
+if [ $? -ne 0 ];
+then
+    exit 1
+else
+    echo "build success!"
+fi
+
 make -j4 install DESTDIR=`pwd`/Inst
+
+
+#adb push Inst/data/local /data/local/tmp/
+#export VALGRIND_LIB=/data/local/tmp/local/Inst/lib/valgrind
+#/data/local/tmp/local/Inst/bin/valgrind --log-file=/sdcard/leak.log --leak-check=full --undef-value-errors=no /data/local/tmp/kcg
